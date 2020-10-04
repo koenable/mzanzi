4f3cc2ca00dac0270809a026737554717891271b
Baholo <mokoenabaholo90@gmail.com>
2020-10-04 02:15:08 +0200

    Successfully inpmorted the Staticts.module.ts/Domain module inot the homeComponenet.

--- dashb/dashboard/src/app/core/components/statistics-card/statistics-card.module.ts
+++ dashb/dashboard/src/app/core/components/statistics-card/statistics-card.module.ts
@@ -1,9 +1,9 @@
-import { StatisticsCardComponent } from './statistics-card.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule} from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import  {MatMenuModule} from '@angular/material/menu';
+import { StatisticsCardComponent } from './statistics-card.component';
 
 
 
--- dashb/dashboard/src/app/pages/home-page/home-page.component.ts
+++ dashb/dashboard/src/app/pages/home-page/home-page.component.ts
@@ -16,23 +16,23 @@ export class HomePageComponent implements OnInit {
   private test= 67;
 
     /** Based on the screen size, switch from standard to one column per row */
-    cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
       map(({ matches }) => {
         if (matches) {
-          return [
-            { title: 'Card 1', cols: 1, rows: 1 },
-            { title: 'Card 2', cols: 1, rows: 1 },
-            { title: 'Card 3', cols: 1, rows: 1 },
-            { title: 'Card 4', cols: 1, rows: 1 }
-          ];
+          return {
+            columns: 1,
+            miniCard: { cols: 1, rows: 1 },
+            chart: { cols: 1, rows: 2 },
+            table: { cols: 1, rows: 4 },
+          };
         }
 
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
+       return {
+          columns: 4,
+          miniCard: { cols: 1, rows: 1 },
+          chart: { cols: 2, rows: 2 },
+          table: { cols: 4, rows: 4 },
+        };
       })
     );
 
